98274e59e5e58b162b22ace5fe1e001881e9ad98
Guilherme Sá <gsa@inoa.com.br>
2022-09-04 23:34:54 -0300

    init

---
+++ Compiler/LexicalHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Compiler {
+    public class LexicalHelper {
+        public struct t_const {
+            public byte Type;
+            public char CharValue;
+            public int NumberValue;
+            public string StringValue;
+        };
+    }
+}
---
+++ Compiler/Program.cs
@@ -0,0 +1,11 @@
+﻿using Compiler;
+
+var file = File.OpenRead("./Program.txt");
+var analyzer = new LexicalAnalyzer(file);
+var isGood = analyzer.Analyze();
+if (!isGood) {
+    Console.WriteLine("There are {0} errors", analyzer.ErrorsCount);
+    return;
+}
+
+Console.WriteLine("The syntax is fine");
